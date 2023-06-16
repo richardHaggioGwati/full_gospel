bf964fda797e64d990f696e83f6ccd8b1f148862
Richard Gwati <haggio0844@gmail.com>
2023-06-16 16:33:27 +0200

    feat(home): refactor

--- container/Home/Ministry/AreaOfMinistry.tsx
+++ container/Home/Ministry/AreaOfMinistry.tsx
@@ -2,52 +2,49 @@ import Image from 'next/image';
 
 import { SubHeading, MinistryItem } from '../../../components';
 import { data, images } from '../../../constants';
-import './AreaOfMinistry.css';
+import styles from '../../Shared/TwoColumSection.module.css';
 
 const AreaOfMinistry = () => (
-  <div className="app__specialMenu flex__center section__padding" id="ministry">
-    <div className="app__specialMenu-title">
+  <div
+    className={`${styles.app__split_col} flex__center section__padding`}
+    id="ministry"
+  >
+    <div className={styles.app__split_col_title}>
       <SubHeading title="Jesus Christ that saves all." />
       <h1 className="headtext__cormorant">AREAS OF MINISTRY</h1>
     </div>
 
-    <div className="app__specialMenu-menu">
-      <div className="app__specialMenu-menu_wine  flex__center">
-        <p className="app__specialMenu-menu_heading">Evangelism</p>
-        <div className="app__specialMenu_menu_items">
+    <div className={styles.app__split_col_section}>
+      <div className={`${styles.app__split_col_section_left}  flex__center`}>
+        <p className={styles.app__split_col_section_heading}>Evangelism</p>
+        <div className={styles.app__split_col_menu_items}>
           {data.evangelism.map((evan, index) => (
             <MinistryItem key={`${evan.title + index}`} prop={evan} />
           ))}
         </div>
       </div>
 
-      <div className="app__specialMenu-menu_img">
+      <div className={styles.app__split_col_section_img}>
         <Image
           src={images.menu}
           style={{
             maxWidth: '100%',
             height: 'auto',
           }}
-          alt="menu__img"
+          alt="section_img"
         />
       </div>
 
-      <div className="app__specialMenu-menu_cocktails  flex__center">
-        <p className="app__specialMenu-menu_heading">
+      <div className={`${styles.app__split_col_section_right}  flex__center`}>
+        <p className={styles.app__split_col_section_heading}>
           Rooted Praise and Worship
         </p>
-        <div className="app__specialMenu_menu_items">
+        <div className={styles.app__split_col_menu_items}>
           {data.praise.map((praises, index) => (
             <MinistryItem key={`${praises.title + index}`} prop={praises} />
           ))}
         </div>
       </div>
-    </div>
-
-    <div style={{ marginTop: 15 }}>
-      <button type="button" className="custom__button">
-        Learn More
-      </button>
     </div>
   </div>
 );
